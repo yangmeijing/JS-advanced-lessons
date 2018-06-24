9c3dacf607f5a75a93b6ecdb924f849146f5fc49
yangmeijing <1806102644@qq.com>
2018-06-24 17:55:29 +0800

    函数扩展

---
+++ LS30/demo.js
@@ -0,0 +1,118 @@
+var f = v => v + 1; //单参数可以不用（），单语句可以不用return关键字
+//var f = (v) => {return v + 1;};
+f(2);//返回3
+
+
+var f = () => 5;
+// 等同于
+var f = function () {
+    return 5
+};
+
+var foo = (num1, num2) => {
+    if (num1 > num2) {
+        return num1 * 2;
+    } else {
+        return num2 * 2;
+    }
+};
+//foo(2,3);
+//foo(3,2);
+
+//箭头函数可以与变量解构结合使用
+const full = ({ first, last }) => last + ' ' + first;
+full({first:"Ming",last:"Li"});
+
+
+//this缺陷
+var point = {
+    x:0,
+    y:0,
+    moveTo:function (x,y) {
+        //内部嵌套函数
+        function moveToX() {
+            this.x = x;//this绑定到了哪里？
+        }
+        //内部嵌套函数
+        function moveToY() {
+            this.y = y;//this绑定到了哪里？
+        }
+        moveToX();
+        moveToY();
+    }
+};
+point.moveTo(2,2);
+console.log(point);
+//console.log(window.x,window.y);
+
+//ES6中 箭头函数中this是与函数定义时所在的对象绑定，而不是使用时所在的对象（避免this缺陷）
+//箭头函数导致this总是指向函数定义生效时所在的对象
+var point = {
+    x:0,
+    y:0,
+    moveTo:function (x,y) {
+        //内部嵌套函数
+        var moveToX = ()=>this.x=x;
+        //内部嵌套函数
+        var moveToY = ()=>this.y=y;
+        moveToX();
+        moveToY();
+    }
+};
+point.moveTo(2,2);
+console.log(point);
+//console.log(window.x,window.y);
+
+// 箭头函数有几个使用注意点。
+// （1）函数体内的this对象，就是定义时所在的对象，而不是使用时所在的对象。
+// （2）不可以当作构造函数，也就是说，不可以使用new命令，否则会抛出一个错误。
+// （3）不可以使用arguments对象，该对象在函数体内不存在。如果要用，可以用Rest参数代替。
+// （4）不可以使用yield命令，因此箭头函数不能用作Generator函数
+
+// ES6 中实现函数参数默认值的方法 ,使用babble查看ES5的写法
+var sum = function(a,b=4,c=5){
+    return a+b+c;
+};
+console.log(sum(1,2,3));//1+2+3
+console.log(sum(1,2));//1+2+5
+console.log(sum(1));//1+4+5
+console.log(sum(1,0,0));//1+0+0
+
+function foo(x=5){
+    let x = 1;//报错
+    const x = 2;//报错
+    var x = 3;//正常
+}
+foo();
+
+//默认值顺序，参数一般有顺序，有默认值的参数应该是尾参数
+//否则无法使有默认值的用默认值，没有默认值的用传递的参数
+function f(x = 1,y) {
+    return [x,y];
+}
+f();//[1,undefined]
+f(2);//[2,undefined]
+//f(,3);//报错，无法使x用1，y用3
+
+
+//所以有默认值的参数在最后
+function f(x,y = 1) {
+    return [x,y];
+}
+f();//[undefined, 1]
+f(2);//[2, 1] 这样就可以x为传递的参数，y为默认的值
+
+//...Rest 相当于合并若干参数为一个数组，主要用于函数定义时，代替 arguments，组解决arguments的弊端
+function f(...y){
+    console.log(y);
+}
+f("a","b","c");//输出 ["a","b","c"]
+
+
+//...Spread 扩展操作符 相当于解数组为分散的参数，主要用于函数调用时，...Rest的逆运算
+function f(x,...y){
+    console.log(x,y);
+}
+f("a",...["b","c"]);//等价于f("a","b","c");
+f("a");//输出 "a",[]
+f();//输出 undefined,[]
